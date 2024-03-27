9ecc4057712a422a31e250200bb6df7d43fcd24e
Felipe Spengler <f@s>
2024-03-27 13:52:47 -0300

    Update
    - Code cleanup
    - env ignore

--- src/main.jsx
+++ src/main.jsx
@@ -2,23 +2,18 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
 
 import { store } from './store/store.js';
-
 import App from './App.jsx'
 
 import './index.scss'
 
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      {/* <PersistGate loading={null} persistor={persistor}> */}
         <BrowserRouter basename='/clothing-store'>
           <App />
         </BrowserRouter>
-      {/* </PersistGate> */}
     </Provider>
   </React.StrictMode>,
 )
